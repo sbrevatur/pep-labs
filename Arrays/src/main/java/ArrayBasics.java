994c5042ecfbd5ab9f7b5215b5dc0ade13412a57
sbrevatur <sberly1@yahoo.com>
2022-12-29 17:44:43 +0000

    Saved progress on Arrays 2022-12-29 17:44:43 +0000

--- Arrays/src/main/java/ArrayBasics.java
+++ Arrays/src/main/java/ArrayBasics.java
@@ -15,10 +15,9 @@ public class ArrayBasics {
      * @return the length of arr.
      */
     public int getLengthOfArray(int[] arr){ 
-           int lenghthofarray = arr.length;
-           return lengthofarray;
+        return arr.length;
+              
     }
-
 
 
     /**
@@ -28,7 +27,7 @@ public class ArrayBasics {
      * @return the element at index n of arr.
      */
     public int getNthElementOfArray(int[] arr, int n){
-        return 0;
+        return arr [n];
     }
 
     /**
@@ -40,7 +39,7 @@ public class ArrayBasics {
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
     public void setNthElementOfArray(int[] arr, int n, int val){
-
+        arr [n] = val;
     }
 
     /**
@@ -48,6 +47,9 @@ public class ArrayBasics {
      * @return a new int array that can contain n elements.
      */
     public int[] returnNewArraySizeN(int n){
-        return null;
+        int[] arr = new int[n];
+        return arr;
+      
     }
 }
+
