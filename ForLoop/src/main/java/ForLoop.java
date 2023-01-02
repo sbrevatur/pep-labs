0f3ba5b6617ea2a05147c78b4389b11664ca5f54
sbrevatur <sberly1@yahoo.com>
2023-01-02 23:20:23 +0000

    Saved progress on ForLoop 2023-01-02 23:20:23 +0000

--- ForLoop/src/main/java/ForLoop.java
+++ ForLoop/src/main/java/ForLoop.java
@@ -42,8 +42,11 @@ public class ForLoop {
     public String returnNumbers(int start, int end){
         String buildingString = "";
 
-//        code here
-
+//        code 
+    for(int i = start; i<end; i++) {
+        buildingString = buildingString + i + " ";
+    }
         return buildingString;
     }
+
 }
