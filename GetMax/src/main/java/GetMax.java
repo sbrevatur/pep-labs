8b3b3c0476a65aa3814839e2ad4324669b88d8be
sbrevatur <sberly1@yahoo.com>
2023-01-14 06:38:31 +0000

    Saved progress on GetMax 2023-01-14 06:38:31 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,14 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxVal = arr [0];
+        for (int a: arr){
+            if(a > maxVal)
+              maxVal = a;
+
+        }
+        return maxVal;
     }
+
 }
+
