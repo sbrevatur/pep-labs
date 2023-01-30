a7760e9792b9b89b4b88c17cb70a04ac877aed26
sbrevatur <sberly1@yahoo.com>
2023-01-30 21:53:12 +0000

    Saved progress on Pyramid 2023-01-30 21:53:12 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,16 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+       String x="";
+        for (int i = 0; i < n; i++) {
+        
+            for (int j = 0; j <= i; j++) {
+                x= x+"*";
+            }
+            x= x +"\n";
+
+
+        }
+        return x;
     }
 }
