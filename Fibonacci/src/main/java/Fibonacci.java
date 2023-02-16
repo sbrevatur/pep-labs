a53ab8547cb5666395a31d15201daf31c1f4bc77
sbrevatur <sberly1@yahoo.com>
2023-02-16 05:58:24 +0000

    Saved progress on Fibonacci 2023-02-16 05:58:24 +0000

--- Fibonacci/src/main/java/Fibonacci.java
+++ Fibonacci/src/main/java/Fibonacci.java
@@ -15,6 +15,9 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        if ((n == 0) || (n == 1))
+           return n;
+        else
+          return fib(n - 1) + fib(n - 2);
     }
 }
