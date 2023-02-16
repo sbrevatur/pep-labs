d9bacbccec9cb4534bb74b8dc1eca8897e1d705b
sbrevatur <sberly1@yahoo.com>
2023-02-16 06:01:42 +0000

    Saved progress on StringIsPalindrome 2023-02-16 06:01:42 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,6 +12,14 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+       String rev = "";
+        boolean ans = false;
+        for (int i = str.length() - 1; i >= 0; i--) {
+        rev = rev + str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            ans = true;
+        }
+        return ans;
     }
 }
